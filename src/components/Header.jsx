11f9510b42faf1b732c158560b030de87e2c34df
Carlos <cvanegashdz@gmail.com>
2024-01-02 10:44:14 -0600

    Create App header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="w-full h-[50px] bg-purple-300">
+        <h1 className="brand">
+            The Meme Blog
+        </h1>
+    </div>
+  )
+}
+
+export default Header
