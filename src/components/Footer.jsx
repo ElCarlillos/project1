d1ac02040b7169780a81e097b1fe4128d05780df
Carlos <cvanegashdz@gmail.com>
2024-01-02 11:06:12 -0600

    Add tailwindcss for styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+  return (
+    <footer className="w-full h-[100px] bg-linear-gradient to-bottom ">
+        <p>App created by Carlos Vanegas. All rigths reserved @2023</p>
+    </footer>
+  )
+}
+
+export default Footer
