010240b168ccffd1fffb0cb8462888b160cf5e0e
Carlos <cvanegashdz@gmail.com>
2024-01-02 11:11:23 -0600

    Add footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 function Footer() {
   return (
-    <footer className="w-full h-[100px] bg-linear-gradient to-bottom ">
+    <footer className="w-full h-[100px] bg-gradient-to-b from-purple-400 to-blue-500 ">
         <p>App created by Carlos Vanegas. All rigths reserved @2023</p>
     </footer>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="w-full h-[50px] bg-purple-300">
+    <div className="w-full h-[50px] bg-blue-700 text-white font-semibold">
         <h1 className="brand">
             The Meme Blog
         </h1>
